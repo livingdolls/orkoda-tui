9f0f7a08e8ec8f05beeb6a42b98ace6d5022dfae
Setiawan <56589776+livingdolls@users.noreply.github.com>
2026-07-31 15:37:26 +0700

    feat: configure scheduler defaults

--- internal/scheduler/defaults.go
+++ internal/scheduler/defaults.go
@@ -1,1 +1,13 @@
 package scheduler
+
+import "time"
+
+func DefaultConfig(workerID string) Config {
+	return Config{
+		WorkerID:      workerID,
+		PollInterval:  250 * time.Millisecond,
+		StaleAfter:    5 * time.Minute,
+		RetryBase:     time.Second,
+		MaxRetryDelay: time.Minute,
+	}
+}
